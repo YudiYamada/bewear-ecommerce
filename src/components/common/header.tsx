91b6ae1e816435ccd1875b458129354629d3d1f9
Yudi Yamada <yudiyamada10@gmail.com>
2025-08-18 18:24:48 -0300

    style: add desktop responsiveness to header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import { LogInIcon, LogOutIcon, MenuIcon, Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -22,16 +22,105 @@ export const Header = () => {
   return (
     <header>
       {/*mobile layout */}
-      <div className="flex items-center justify-between p-5">
-        <Link href="/">
-          <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
-        </Link>
+      <div className="md:hidden">
+        <div className="flex items-center justify-between p-5">
+          <Link href="/">
+            <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+          </Link>
 
-        <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <MenuIcon />
+                </Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Menu</SheetTitle>
+                </SheetHeader>
+                <div className="px-5">
+                  {session?.user ? (
+                    <>
+                      <div className="flex justify-between space-y-6">
+                        <div className="flex items-center gap-3">
+                          <Avatar>
+                            <AvatarImage
+                              src={session?.user?.image as string | undefined}
+                            />
+                            <AvatarFallback>
+                              {session?.user?.name?.split(" ")?.[0]?.[0]}
+                              {session?.user?.name?.split(" ")?.[1]?.[0]}
+                            </AvatarFallback>
+                          </Avatar>
+
+                          <div>
+                            <h3 className="font-semibold">
+                              {session?.user?.name}
+                            </h3>
+                            <span className="text-muted-foreground block text-xs">
+                              {session?.user?.email}
+                            </span>
+                          </div>
+                        </div>
+                        <Button
+                          variant="outline"
+                          size="icon"
+                          onClick={() => authClient.signOut()}
+                        >
+                          <LogOutIcon />
+                        </Button>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="flex items-center justify-between">
+                      <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                      <Button size="icon" asChild variant="outline">
+                        <Link href="/authentication">
+                          <LogInIcon />
+                        </Link>
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              </SheetContent>
+            </Sheet>
+            <Cart />
+          </div>
+        </div>
+      </div>
+
+      {/*desktop layout */}
+      <div className="hidden items-center justify-between p-5 md:flex">
+        <div>
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon />
+              <Button variant="outline" className="cursor-pointer border-none">
+                {session?.user ? (
+                  <>
+                    <div className="flex items-center gap-3">
+                      <Avatar>
+                        <AvatarImage
+                          src={session?.user?.image as string | undefined}
+                        />
+                        <AvatarFallback>
+                          {session?.user?.name?.split(" ")?.[0]?.[0]}
+                          {session?.user?.name?.split(" ")?.[1]?.[0]}
+                        </AvatarFallback>
+                      </Avatar>
+
+                      <div>
+                        <h3 className="font-semibold">{session?.user?.name}</h3>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div>
+                    <Link href="/authentication">
+                      <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                    </Link>
+                  </div>
+                )}
               </Button>
             </SheetTrigger>
             <SheetContent>
@@ -66,6 +155,7 @@ export const Header = () => {
                         variant="outline"
                         size="icon"
                         onClick={() => authClient.signOut()}
+                        className="cursor-pointer"
                       >
                         <LogOutIcon />
                       </Button>
@@ -74,7 +164,12 @@ export const Header = () => {
                 ) : (
                   <div className="flex items-center justify-between">
                     <h2 className="font-semibold">Olá. Faça seu login!</h2>
-                    <Button size="icon" asChild variant="outline">
+                    <Button
+                      size="icon"
+                      asChild
+                      variant="outline"
+                      className="cursor-pointer"
+                    >
                       <Link href="/authentication">
                         <LogInIcon />
                       </Link>
@@ -84,7 +179,20 @@ export const Header = () => {
               </div>
             </SheetContent>
           </Sheet>
-          <Cart />
+        </div>
+
+        <div>
+          <Link href="/">
+            <Image src="/logo.svg" alt="BEWEAR" width={100} height={26.14} />
+          </Link>
+        </div>
+        <div className="flex items-center gap-2">
+          <div>
+            <Search />
+          </div>
+          <div>
+            <Cart />
+          </div>
         </div>
       </div>
     </header>
