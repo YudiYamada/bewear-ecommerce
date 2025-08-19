c392427622bceba4618d5d9b9214d147b29fb53a
Yudi Yamada <yudiyamada10@gmail.com>
2025-08-19 16:27:04 -0300

    feat: add desktop responsiveness to login screen

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
@@ -8,8 +10,8 @@ const Authentication = async () => {
   return (
     <>
       <Header />
-
-      <div className="flex w-full flex-col gap-6 p-5">
+      {/*mobile*/}
+      <div className="flex w-full flex-col gap-6 p-5 md:hidden">
         <Tabs defaultValue="sign-in">
           <TabsList>
             <TabsTrigger value="sign-in">Entrar</TabsTrigger>
@@ -22,6 +24,34 @@ const Authentication = async () => {
             <SignUpForm />
           </TabsContent>
         </Tabs>
+      </div>
+
+      {/*desktop*/}
+      <div className="mx-auto mt-6 hidden max-w-[1440px] items-center justify-center md:flex">
+        <div>
+          <Image
+            src="/banner.jpg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="w-[500px] rounded-2xl"
+          />
+        </div>
+        <div className="flex w-[500px] flex-col gap-6 p-5">
+          <Tabs defaultValue="sign-in">
+            <TabsList>
+              <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+              <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+            </TabsList>
+            <TabsContent value="sign-in" className="w-full">
+              <SignInForm />
+            </TabsContent>
+            <TabsContent value="sign-up" className="w-full">
+              <SignUpForm />
+            </TabsContent>
+          </Tabs>
+        </div>
       </div>
     </>
   );
